7954a9b46f34f186a562fbcc562a1d5bc422134a
LiverMalfunction <b06901096@g.ntu.edu.tw>
2021-01-09 16:48:03 +0800

    LDPC SER simulation

--- code/5g-nr-ldpc-master/ldpcExample.m
+++ code/5g-nr-ldpc-master/ldpcExample.m
@@ -11,22 +11,73 @@ end
 M = 4;
 
 % LDPC config
-blkSize = 256;
-codeRate = '5/6';
+blkSize = 512;
+codeRate = '3/4';
 
 % Get LDPC struct
 LDPC = ldpcGet(blkSize, codeRate);
-
+%{
+puncture_num = 46;
+weight = zeros(1, 512);
+parfor bit_flip = 1:512
+    bit_flip
+    codeword = zeros(1, 558);
+    codeword(bit_flip+puncture_num) = 1;
+    syndrome = mod((codeword*LDPC.H'),2);
+    while sum(syndrome) ~= 0
+        if sum(codeword) == 512
+            break;
+        end
+        position = [];
+        for idx = 1:162
+            if syndrome(idx) == 1
+                position = [position idx];
+            end
+        end
+        flip_idx = 0;
+        max_flip = 0;
+        min_extra_flip = 162;
+        for idx = 1:512
+            if codeword(idx+puncture_num) == 0
+                flip = 0;
+                extra_flip = 0;
+                for jdx = 1:162
+                    if LDPC.H(jdx,idx+puncture_num) == 1
+                        if ismember(jdx,position)
+                            flip = flip + 1;
+                        else
+                            extra_flip = extra_flip + 1;
+                        end
+                    end
+                end
+                if flip > max_flip
+                    flip_idx = idx;
+                    max_flip = flip;
+                    min_extra_flip = extra_flip;
+                elseif flip == max_flip && min_extra_flip > extra_flip
+                    flip_idx = idx;
+                    max_flip = flip;
+                    min_extra_flip = extra_flip;
+                end
+            end
+        end
+        codeword(flip_idx+puncture_num) = 1;
+        syndrome = mod((codeword*LDPC.H'),2);
+    end
+    sum(codeword)
+    weight(bit_flip) = sum(codeword);
+end
+%}
 % Simulation parameters
-ebno = 8;
-numIter = 1e2;
+ebno = 100000;
+numIter = 1e6;
 numErr = 0;
-
+cntErr = 0;
 % Convert E_b/N_0 to some SNR
 snr = ebno + 10*log10(log2(M)) + 10*log10(str2num(codeRate));
 
 %% Simulate
-for i = 1:numIter
+parfor i = 1:numIter
     
     % Generate random data
     data = randi([0 1], 1, LDPC.numInfBits);
@@ -48,8 +99,15 @@ for i = 1:numIter
 
     % Count number of bit errors
     numErr = numErr + sum(abs(dataHat - data));
+
+    % Count number of error symbols
+    if sum(abs(dataHat - data)) ~= 0
+        cntErr = cntErr + 1;
+    end
     
 end
 
 %% BER
 ber = numErr / (numIter * LDPC.numInfBits)
+%% SER
+ser = cntErr/ numIter
